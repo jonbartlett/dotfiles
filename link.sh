13ded9baa8132abd2df7c153f9e304d21e840e0f
Jon Bartlett <mail@jaybe.net>
2017-01-12 11:12:44 +1100

    big re-org to use gnu stow

--- link.sh
+++ link.sh
@@ -4,39 +4,25 @@
 # Script to sym link dotfile from this Git Repo
 #  lastest version: https://github.com/jonbartlett/dotfiles/blob/master/link.sh
 #
+# for d in `ls .`;
+# do
+#   ( stow $d )
+# done
+#
 
-# vim
-ln -s "$(pwd)/.vimrc" ~/.vimrc
+stow stow
+stow vim
+stow git
+stow screen
+stow inputrc
+stow tmux
 
-# vim spell file
-ln -s "$(pwd)/vimspellfile-en.utf-8.add" ~/vimspellfile-en.utf-8.add
-
-# git config
-ln -s "$(pwd)/.gitconfig" ~/.gitconfig
-ln -s "$(pwd)/.gitignore_global" ~/.gitignore_global
-
-# screen
-ln -s "$(pwd)/.screenrc" ~/.screenrc
-
-# Bash Readline
-ln -s "$(pwd)/.inputrc" ~/.inputrc
-
-# TMUX
-ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf
-
-# simplenote.vim
-cp "$(pwd)/.simplenoterc" ~/.simplenoterc
-
-#svn config # note this contains hardcoded reference to HOME dir
-mkdir ~/.subversion
-ln -s "$(pwd)/svn_config" ~/.subversion/config
-
-ln -s "$(pwd)/svnvimdiff.sh" ~/.subversion/svnvimdiff.sh
-chmod +x "$(pwd)/svnvimdiff.sh"
+chmod +x subversion/.subversion/svnvimdiff.sh
+rm ~/.subversion/config; stow subversion
 
 # ssh config
 if [ ! -d "$~/.ssh" ]; then
-  mkdir ~/.subversion
+  mkdir ~/.ssh
 fi
 
 ln -s "$(pwd)/ssh_config" ~/.ssh/config
